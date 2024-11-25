ee54eb4587db6a9edafbeee55a7cbc80e34d0386
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-11-25 19:36:44 -0300

    cOMMIT WEEKS 44 & 45

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 42',
-  data: [15, 15, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 44',
+  data: [10, 17, 27], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -14,10 +14,10 @@ const dataLine1 = {
 };
 
 const dataLine2 = {
-  labels: ['Día 1', 'Día 2', 'Día 3','Día 4', 'Día 5'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 43',
-    data: [17, 10, 8, 10, 18], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 45',
+    data: [16, 14, 22], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
